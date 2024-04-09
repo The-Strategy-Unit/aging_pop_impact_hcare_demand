f1b32bb16ff616b631e197d1b0bd0d85dea34a72
paulseamer <paulseamer@nhs.net>
2024-04-09 11:46:34 +0100

    Refactor: update dataset structure for Tim Brock
    Update result dataset structure to work with the new D3 codebase
    being developed by Tim Brock. Includes adding modal results from HSA
    model.

--- R/assemble_result_inputs.R
+++ R/assemble_result_inputs.R
@@ -8,22 +8,26 @@ library("here")
 library("jsonlite")
 library("purrr")
 library("stringr")
+library("tictoc")
 
 # helpers ----
-source(here("R", "helper_utility_fns.R"))
-source(here("R", "helper_app_inputs.R"))
 source(here("R", "hsa_run_model.R"))
 source(here("R", "hsa_assemble_results.R"))
-
-# run the model for a single area ----
+source(here("R", "helper_utility_fns.R"))
+source(here("R", "helper_app_inputs.R"))
 load_proj_lookup()
-area_code <- "E09000030"
 
+# read ----
+area_codes_df <- read_csv(
+  here("data", "app_inputs", "area_names_and_codes.csv"),
+  show_col_types = FALSE
+)
+area_code <- sort(unique(area_codes_df$cd))
+
+# run models ----
 # run models for this set of parameters
-model_params <- lookup_proj |>
-  mutate(area_code = area_code, .before = everything()) |>
+model_params <- expand_grid(lookup_proj, area_code) |>
   filter(proj_id %in% vars_app) |>
-  mutate(proj_id = factor(proj_id, levels = vars_app)) |>
   select(area_code, proj = proj_id) |>
   group_by(across(everything())) |>
   expand(base_year = 2022L, end_year = seq(2025L, 2040L, 5L)) |>
@@ -33,68 +37,151 @@ model_params <- lookup_proj |>
   ) |>
   ungroup()
 
-demo_res <- pmap(model_params |> select(-model_runs, -rng_state), demo_fac)
-hsa_res <- pmap(model_params, hsa_fac)
+# 360 areas x 11 variants x 4 end years = 15,840 models
+# plus x1000 simulations for the full model
+# 3 versions of the model
+# i) pure demographic model
+# ii) hsa mode only model
+# iii) hsa full model
 
-# assemble results for a single area ----
+# i) pure demographic model
+model_params_nosim  <- model_params |>
+  select(-model_runs, -rng_state)
+
+tic.clearlog()
+tic("Pure demographic model")
+demo_res <- pmap(
+  model_params_nosim,
+  demo_fac,
+  # add a progress bar
+  .progress = list(
+    clear = TRUE,
+    format = "Pure demographic model: {cli::pb_bar} {cli::pb_percent} | ETA: {cli::pb_eta}", # nolint: line_length_linter.
+    type = "iterator"
+  )
+)
+toc(log = TRUE, quiet = TRUE)
+
+# ii) hsa mode only model
+tic("HSA mode only model")
+hsa_mode_res <- pmap(
+  model_params_nosim,
+  hsa_mode_fac,
+  method = "interp",
+  .progress = list(
+    clear = TRUE,
+    format = "HSA mode only model: {cli::pb_bar} {cli::pb_percent} | ETA: {cli::pb_eta}", # nolint: line_length_linter.
+    type = "iterator"
+  )
+)
+toc(log = TRUE, quiet = TRUE)
+
+# iii) hsa full model
+tic("HSA full model")
+hsa_res <- pmap(
+  model_params,
+  hsa_fac,
+  method = "interp",
+  .progress = list(
+    clear = TRUE,
+    format = "HSA full model: {cli::pb_bar} {cli::pb_percent} | ETA: {cli::pb_eta}", # nolint: line_length_linter.
+    type = "iterator"
+  )
+)
+toc(log = TRUE, quiet = TRUE)
+
+# save timings
+timings_run <- tic.log(format = TRUE)
+timings_run |>
+  enframe() |>
+  unnest(cols = c(value)) |>
+  write_csv(here("R", "timings_model_runs.csv"))
+
+# assemble  ----
+# group results for sex
 demo_res <- map(demo_res, grp_sex_demo)
+hsa_mode_res <- map(hsa_mode_res, grp_sex_hsa_mode)
 hsa_res <- map(hsa_res, grp_sex_hsa)
 
-demo_res <- attach_params(demo_res)
-hsa_res <- attach_params(hsa_res)
+# make a single list
+res_ls <- pmap(
+  list(demo_res, hsa_mode_res, hsa_res),
+  \(x, y, z) {
+    x |>
+      left_join(y, join_by(hsagrp)) |>
+      left_join(z, join_by(hsagrp))
+  }
+)
 
-demo_res <- bind_rows(demo_res, .id = "id")
-hsa_res <- bind_rows(hsa_res, .id = "id")
+# format df ready to save as JSON
+format_json <- function(df_res) {
+  df_res |>
+    select(-(ends_with("_n"))) |>
+    unnest(end_p) |>
+    left_join(hsagrp_labs, join_by(hsagrp)) |>
+    mutate(
+      pod = str_sub(hsagrp, 1, 3), hsagrp = str_sub(hsagrp, 5, -1L),
+      hsagrp = factor(hsagrp, levels = hsagrp_levels)
+    ) |>
+    rename(group = hsagrp, label = hsagrp_lab) |>
+    mutate(
+      end_p = 100 * end_p - 100,
+      end_p_nohsa = 100 * end_p_nohsa - 100
+    ) |>
+    select(pod, group, label, end_p, end_p_md, end_p_nohsa)
+}
 
-res <- hsa_res |>
-  left_join(
-    demo_res,
-    join_by("id", "area_code", "hsagrp", "proj_var", "end_year")
-  )
-
-res <- res |>
-  select(-(ends_with("_n"))) |>
-  mutate(
-    proj_id = as.numeric(proj_var),
-    id = as.numeric(id)
-  ) |>
-  unnest(end_p) |>
-  left_join(hsagrp_labs, join_by("hsagrp")) |>
-  mutate(
-    pod = str_sub(hsagrp, 1, 3),
-    hsagrp = str_sub(hsagrp, 5, -1L),
-    hsagrp = factor(hsagrp, levels = hsagrp_levels),
-    end_p = 100 * end_p - 100,
-    end_p_nohsa = 100 * end_p_nohsa - 100
-  ) |>
-  select(
-    area_code,
-    id,
-    proj_id,
-    proj_var,
-    end_year,
-    pod,
-    hsagrp,
-    hsagrp_lab,
-    end_p,
-    end_p_nohsa
-  ) |>
-  arrange(id)
+res_ls <- map(res_ls, \(x) format_json(x))
 
 # compute binning ----
-json_res <- res |>
-  group_by(across(-end_p)) |>
-  nest() |>
-  arrange(id) |>
-  mutate(data = map(data, \(x) pretty_fd(x))) |>
-  ungroup()
+comp_bins <- function(df_res) {
+  df_res |>
+    group_by(across(-end_p)) |>
+    nest(.key = "data") |>
+    mutate(data = map(data, \(x) pretty_fd(x))) |>
+    ungroup()
+}
+
+res_ls <- map(
+  res_ls,
+  \(x) {
+    comp_bins(x)
+  },
+  .progress = list(
+    clear = TRUE,
+    format = "Compute bins: {cli::pb_bar} {cli::pb_percent} | ETA: {cli::pb_eta}", # nolint: line_length_linter.
+    type = "iterator"
+  )
+)
+
+# attach params ----
+params_ls <- split(
+  model_params_nosim |>
+    select(-base_year) |>
+    rename(variant = proj) |>
+    mutate(
+      variant = factor(variant, levels = vars_levels),
+      variant = paste0("v", as.integer(variant))
+    )
+  , seq_len(nrow(model_params_nosim))
+)
+
+app_ls <- map2(res_ls, params_ls, \(x, y) bind_cols(y, x))
+names(app_ls) <- map(app_ls, \(x) pluck(x$area_code, 1))
+app_ls <- split(app_ls, names(app_ls)) |> map(bind_rows)
 
 # save ----
-jsonlite::write_json(
-  json_res,
-  here(
-    "data",
-    "app_inputs",
-    paste0("test_results_", json_res$area_code[[1]], ".json")
-  )
+walk2(
+  app_ls, names(app_ls),
+  \(x, y) {
+    jsonlite::write_json(
+      x,
+      here(
+        "data",
+        "app_inputs",
+        "histograms",
+        paste0(y, ".json")
+      )
+    )
+  }
 )
